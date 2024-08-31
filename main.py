bcdedf54b43052f736c001ab2b5486a2d85ca99e
massivebird <gdrakemail@gmail.com>
2024-08-31 17:20:53 -0400

    feat: matching systems, colored output

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re, os, yaml
+import re, os, yaml, termcolor
 
 archive_root = "/home/penguino/game-archive"
+
+config_file = open(archive_root + "/config.yaml", 'r')
+config_yaml = yaml.safe_load(config_file)
 
 def clean_filename(s: str) -> str:
     s = re.sub(r'\(.*\)|\[.*\]', "", s)
@@ -11,26 +14,34 @@ def clean_filename(s: str) -> str:
 
     return s
 
-config_file = open(archive_root + "/config.yaml", 'r')
-config_yaml = yaml.safe_load(config_file)
+class GameSystem:
+    def __init__(self, path, name, r, g, b):
+        self.path = path
+        self.pretty_string = f"\033[38;2;{r};{g};{b}m{name}\033[0m"
 
-system_labels = list(config_yaml["systems"])
+    def __str__(self) -> str:
+        return self.pretty_string
+
+    def get_path(self) -> str:
+        return self.path
+
+# Compile game systems from archive configuration file
+systems = []
+for label in config_yaml["systems"]:
+    sys = config_yaml['systems'][label]
+    color = sys['color']
+    systems.append(GameSystem(sys['path'], sys['display_name'], color[0], color[1], color[2]))
 
 for root, subDirectory, files in os.walk(archive_root):
+    matching_system = None
+    for system in systems:
+        if f"{archive_root}/{system.path}" in root:
+            matching_system = system
 
-    dividers = re.search(r'/', root)
-
-    if dividers == None:
-        continue # no idea how this could happen
-
-    skip_it = True
-    for label in system_labels:
-        if root.__contains__(str(label)):
-            skip_it = False
-            break
-
-    if skip_it:
+    # If this path does not correspond to a defined system,
+    # then continue to the next path
+    if matching_system == None:
         continue
 
-    for name in files:
-        print(clean_filename(name))
+    for filename in files:
+        print(f"[{matching_system}] " + clean_filename(filename))
